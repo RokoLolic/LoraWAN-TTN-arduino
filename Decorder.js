e9562a247c4822cac7bbff3c689f4588bf87ee71
RokoLolic <73552688+RokoLolic@users.noreply.github.com>
2024-07-12 20:41:10 +0200

    Update Decorder.js

--- Decorder.js
+++ Decorder.js
@@ -1,10 +1,10 @@
 function Decoder(b, port) {
   
   var temp = b[0]; // receive temperatre in range [0, 255], requires a fridge test
-  /*if (temp > 127){
+  if (temp > 127){
     temp = temp - 256; 
-  }*/
-  var gas = b[1] * 1000; //kOhm air resistance, for air pollution
+  }
+  var gas = b[1] * 10; //miliOhm air resistance, for air pollution
   var humidity = b[2]; //humidity in percentage
   var pressure = 870 + b[3]; //870 mb is record lowest pressure, and end device sends difference between current pressure and 870 mb
   return {
